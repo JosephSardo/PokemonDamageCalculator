92bdc1835dae5a812c96c8a4bf0b6c5946bc8a8d
JosephSardo <josephsardo2025@gmail.com>
2026-07-05 13:52:05 -0400

    Initial project setup

---
+++ src/PokemonDamageCalculator/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PokemonDamageCalculator;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
