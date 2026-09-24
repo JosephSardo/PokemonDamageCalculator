fb4fc41552b903cdd51310deb2bde2e75ba22af5
JosephSardo <josephsardo2025@gmail.com>
2026-09-24 16:25:29 -0400

    Fixed incorrect rounding in damage calculation

--- src/PokemonDamageCalculator/Services/StatCalculator.cs/DamageCalculator.cs
+++ src/PokemonDamageCalculator/Services/StatCalculator.cs/DamageCalculator.cs
@@ -7,10 +7,8 @@ public static class DamageCalculator
 {
     public static int calcDamage(Pokemon attacker, Pokemon defender, Move move, BattleState battleState)
     {
-        //To do: don't condense every multiplier into one double (it prevents damage numbers from being truncated in the correct places)
-        double multiplier = 1; //final multiplier calculated then applied to damage to prevent rounding issues
-        double relevantAtk;
-        double relevantDef;
+        int relevantAtk;
+        int relevantDef;
         PokemonType moveType = move.Type;
 
         if(move.Category == MoveCategory.Physical)
@@ -18,69 +16,17 @@ public static class DamageCalculator
             relevantAtk = attacker.FinalStats.Atk;
             relevantDef = defender.FinalStats.Def;
             //determine stat changes
-            relevantAtk = relevantAtk * attacker.getAtkMultiplier();
-            relevantDef = relevantDef * defender.getDefMultiplier();
+            relevantAtk = (int)(relevantAtk * attacker.getAtkMultiplier());
+            relevantDef = (int)(relevantDef * defender.getDefMultiplier());
         } else
         {
             relevantAtk = attacker.FinalStats.SpAtk;
             relevantDef = defender.FinalStats.SpDef;
         }
 
-        //calculate multiplier
-        //determine STAB bonus
-        if(moveType == attacker.Types.Type1 || (attacker.Types.Type2 != null && moveType == attacker.Types.Type2))
-        {
-            multiplier *= 1.5;
-        }
-
-        //determine weather modifier (note: each weather buffs weather ball and changes its type)
+        //determine if weather affects defensive stats (snow or sand)
         Weather weather = battleState.Weather;
 
-        //sun
-        if(weather == Weather.Sun || weather == Weather.HSun)
-        {
-            if(moveType == PokemonType.Fire) //any sun boosts fire attacks
-            {
-                multiplier *= 1.5;
-            } else if(moveType == PokemonType.Water) //regular sun halves water attacks
-            {
-                multiplier *= 0.5;
-                if(weather == Weather.HSun) //extremely harsh sun negates water attacks
-                {
-                    multiplier *= 0;
-                }
-            }
-
-            if(move.Name == "Weather Ball")
-            {
-                multiplier *= 2;
-                moveType = PokemonType.Fire;
-            }
-        }
-
-        //rain
-        if(weather == Weather.Rain || weather == Weather.HRain)
-        {
-            if(moveType == PokemonType.Water) //any rain boosts water attacks
-            {
-                multiplier *= 1.5;
-            } else if(moveType == PokemonType.Fire) //any rain halves fire attacks
-            {
-                multiplier *= 0.5;
-            }
-
-            if(move.Name == "Solar Beam" || move.Name == "Solar Blade") //special case: these 2 moves are nerfed by rain
-            {
-                multiplier *= 0.5;
-            }
-
-            if(move.Name == "Weather Ball")
-            {
-                multiplier *= 2;
-                moveType = PokemonType.Water;
-            }
-        }
-        
         //sand
         if(weather == Weather.Sand)
         {
@@ -92,7 +38,6 @@ public static class DamageCalculator
 
             if(move.Name == "Weather Ball")
             {
-                multiplier *= 2;
                 moveType = PokemonType.Rock;
             }
         }
@@ -108,16 +53,15 @@ public static class DamageCalculator
 
             if(move.Name == "Weather Ball")
             {
-                multiplier *= 2;
                 moveType = PokemonType.Ice;
             }
         }
 
-        //determine type matchup
-        multiplier *= TypeChart.getEffectiveness(move.Type, defender.Types.Type1);
-        if(defender.Types.Type2 != null) //check if the pokemon has a second type before determining its matchup
+        //determine weather ball multiplier
+        int effectiveMovePower = move.Power;
+        if(move.Name == "Weather Ball" && weather != Weather.None)
         {
-            multiplier *= TypeChart.getEffectiveness(move.Type, (PokemonType)defender.Types.Type2);
+            effectiveMovePower *= 2;
         }
 
         //run official damage formula
@@ -126,13 +70,68 @@ public static class DamageCalculator
 
         int damage = (2 * attacker.Level) / 5;
         damage += 2;
-        damage *= move.Power;
+        damage *= effectiveMovePower;
         damage *= (int)relevantAtk;
         damage /= (int)relevantDef;
         damage /= 50;
         damage += 2;
 
-        damage = (int)(damage * multiplier);
+        //determine if weather affects damage (sun or rain)
+        //sun
+        if(weather == Weather.Sun || weather == Weather.HSun)
+        {
+            if(moveType == PokemonType.Fire) //any sun boosts fire attacks
+            {
+                damage = (int)(damage * 1.5);
+            } else if(moveType == PokemonType.Water) //regular sun halves water attacks
+            {
+                damage = (int)(damage * 0.5);
+                if(weather == Weather.HSun) //extremely harsh sun negates water attacks
+                {
+                    return 0;
+                }
+            }
+
+            if(move.Name == "Weather Ball")
+            {
+                moveType = PokemonType.Fire;
+            }
+        }
+
+        //rain
+        if(weather == Weather.Rain || weather == Weather.HRain)
+        {
+            if(moveType == PokemonType.Water) //any rain boosts water attacks
+            {
+                damage = (int)(damage * 1.5);
+            } else if(moveType == PokemonType.Fire) //any rain halves fire attacks
+            {
+                damage = (int)(damage * 0.5);
+            }
+
+            if(move.Name == "Solar Beam" || move.Name == "Solar Blade") //special case: these 2 moves are nerfed by rain
+            {
+                damage = (int)(damage * 0.5);
+            }
+
+            if(move.Name == "Weather Ball")
+            {
+                moveType = PokemonType.Water;
+            }
+        }
+
+        //determine STAB bonus
+        if(moveType == attacker.Types.Type1 || (attacker.Types.Type2 != null && moveType == attacker.Types.Type2))
+        {
+            damage = (int)(damage * 1.5);
+        }
+
+        //determine type matchup
+        damage = (int)(damage * TypeChart.getEffectiveness(move.Type, defender.Types.Type1));
+        if(defender.Types.Type2 != null) //check if the pokemon has a second type before determining its matchup
+        {
+            damage = (int)(damage * TypeChart.getEffectiveness(move.Type, (PokemonType)defender.Types.Type2));
+        }
 
         return damage;
     }
